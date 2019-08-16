8d3a3b1ecb6824ef3fdee5352ef74258262d2c57
Jason <jbpulis@yahoo.com>
2019-08-16 15:07:55 -0600

    final clean up
    clean up the output to display the perteneat info

--- src/emailapp/email.java
+++ src/emailapp/email.java
@@ -17,11 +17,11 @@ public class email {
 		public email(String firstName, String lastName) {
 			this.firstName = firstName;
 			this.lastName = lastName;
-			System.out.println("EMAIL CREATED: " + this.firstName + " " + this.lastName);
+			//System.out.println("EMAIL CREATED: " + this.firstName + " " + this.lastName);
 			
 			// call a method asking for the department - return the department
 			this.department = setDepartment();
-			System.out.println("Department: " + this.department);
+			//System.out.println("Department: " + this.department);
 			
 			//call a method that returns a random password
 			this.password = randomPassword(defaultPasswordLength);
@@ -29,12 +29,12 @@ public class email {
 			
 			// Combine to generate email
 			email = firstName.toLowerCase() + "." + lastName.toLowerCase() + "@" + department + "." + companySuffix;
-			System.out.println("Your email is: " + email);
+			//System.out.println("Your email is: " + email);
 		}
 		
 		// Ask for the department
 		private String setDepartment() {
-			System.out.print("DEPARTMENT CODES\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter department code: ");
+			System.out.print("New worker: " + firstName + ". DEPARTMENT CODES\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter department code: ");
 			Scanner in = new Scanner(System.in);
 			int depChoice = in.nextInt();
 			if(depChoice == 1) {return "sales"; }
@@ -72,5 +72,11 @@ public class email {
 		public int getMailboxCapacity() { return mailboxCapacity; }
 		public String getAlternateEmail() { return alternateEmail; }
 		public String getPassword() { return password; }
+		
+		public String showInfo() {
+			return "DISPLAY NAME: " + firstName + " " + lastName + 
+					"\nCOMPANY EMAIL: " + email +
+					"\nMAILBOX CAPACITY: " + mailboxCapacity + "mb";
+		}
 		
 }
--- src/emailapp/emailApp.java
+++ src/emailapp/emailApp.java
@@ -4,9 +4,10 @@ public class emailApp {
 
 	public static void main(String[] args) {
 		email em1 = new email("John", "Smith");
+		System.out.println(em1.showInfo());
 
-		em1.setAlternateEmail("new@email.com");
-		System.out.println("Alternate email: " + em1.getAlternateEmail());
+		//em1.setAlternateEmail("new@email.com");
+		//System.out.println("Alternate email: " + em1.getAlternateEmail());
 		
 	}
 
