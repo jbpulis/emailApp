4e71ade81ff82f8683c45964c84cf156ffcc8bc8
Jason <jbpulis@yahoo.com>
2019-08-16 13:30:43 -0600

    Update email.java
    created ability to set a department

--- src/emailapp/email.java
+++ src/emailapp/email.java
@@ -1,4 +1,5 @@
 package emailapp;
+import java.util.*;
 
 public class email {
 		private String firstName;
@@ -13,10 +14,22 @@ public class email {
 			this.firstName = firstName;
 			this.lastName = lastName;
 			System.out.println("EMAIL CREATED: " + this.firstName + " " + this.lastName);
+			
+			// call a method asking for the department - return the department
+			this.department = setDepartment();
+			System.out.println("Department: " + this.department);
 		}
 		
 		// Ask for the department
-		
+		private String setDepartment() {
+			System.out.print("DEPARTMENT CODES\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter department code: ");
+			Scanner in = new Scanner(System.in);
+			int depChoice = in.nextInt();
+			if(depChoice == 1) {return "sales"; }
+			else if (depChoice == 2) {return "dev"; }
+			else if (depChoice == 3) {return "acct"; }
+			else {return ""; }
+		}
 		
 		// Generate a random password
 		
